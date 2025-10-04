5c7fdceccd6631bd9d0d2092a4013a44e973fc63
Haroldo Junior <haroldomedeiros203@hotmail.com>
2025-10-04 15:05:02 -0300

    Adding data blocks to data.tf

--- data.tf
+++ data.tf
@@ -1,12 +1,9 @@
 data "kubernetes_service" "app_loadbalancer_service" {
-
   metadata {
     name      = "svc-app-loadbalancer"
     namespace = "default"
   }
-
 }
-
 
 data "aws_eks_cluster" "cluster" {
   name = "ordering-eks-cluster"
@@ -14,4 +11,14 @@ data "aws_eks_cluster" "cluster" {
 
 data "aws_eks_cluster_auth" "auth" {
   name = "ordering-eks-cluster"
+}
+
+data "aws_iam_role" "lab_role" {
+  name = "LabRole"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_authorizer.py"
+  output_path = "${path.module}/lambda_authorizer.zip"
 }
